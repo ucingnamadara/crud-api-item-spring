6c0f355cec831263ce06e2473fb82784ad433c77
Ananda Dana Pratama <ananda.pratama@ifg-life.id>
2023-01-16 21:02:29 +0700

    add new method query

--- src/main/java/id/kawahedukasi/cruditemspring/repository/ItemRepository.java
+++ src/main/java/id/kawahedukasi/cruditemspring/repository/ItemRepository.java
@@ -2,11 +2,16 @@ package id.kawahedukasi.cruditemspring.repository;
 
 import id.kawahedukasi.cruditemspring.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item,String> {
     Optional<Item> findByName(String name); //bikin abstract method
+
+    @Query(nativeQuery = true, value = "SELECT * FROM item_schema.item where \"count\" != 0 ")
+    List<Item> listExistItem();
 }
